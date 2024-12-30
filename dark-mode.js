50414979bb26c5f74abae615ed80e7316cb89a57
Mert Altekin <altekinmert.ceng@gmail.com>
2024-12-30 13:36:37 +0300

    next episode downgrade

--- dark-mode.js
+++ dark-mode.js
@@ -7,6 +7,7 @@
   const quotes = document.querySelector('.quotes');
   const container3_new = document.querySelector('.container3');
   const commentmodal = document.querySelector('.comment-modal');
+  const nextepisode2 = document.querySelector('.next-episode');
 
 	toggle.addEventListener('change', () => {
 		content.classList.toggle('dark-mode');
@@ -16,4 +17,5 @@
     document.querySelectorAll('.quotes').forEach(quote => quote.classList.toggle('dark-mode'));
     container3_new.classList.toggle('dark-mode');
     commentmodal.classList.toggle('dark-mode');
+    nextepisode2.classList.toggle('dark-mode');
 	});
